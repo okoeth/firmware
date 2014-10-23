aba640f69901c6fb9d7ac73f1cff9eb8972ded83
Oliver Koeth <koeth@acm.org>
2014-10-23 21:36:59 +0200

    Alpha release for per health

--- pet-health/pet-health.ino
+++ pet-health/pet-health.ino
@@ -26,9 +26,10 @@
   - LED Sync      - pin 5
   - LED Sleep     - pin 6
   - LED Active    - pin 3
+ 
  */
 
-#include <SD.h>
+//#include <SD.h>
 #include <Wire.h>
 
 // Pin layout 
@@ -66,6 +67,8 @@ void initializePins() {
 	pinMode(PIN_LED_SYNC, OUTPUT);     
 	pinMode(PIN_LED_SLEEP, OUTPUT);
 	pinMode(PIN_LED_ACTIVE, OUTPUT);	
+
+	analogWrite(PIN_LED_ACTIVE, 0);
 }
 
 // Initialize the serial communication
@@ -75,6 +78,7 @@ void initializeSerial() {
 	statusSerial = true;
 }
 
+/*
 // Initialize the SD card
 void initializeSDcard() {
 	// Required for SD card
@@ -87,8 +91,10 @@ void initializeSDcard() {
 	} else {
 		Serial.println("<log message='SD card present...'/>");
 		statusSD = true;
+		logData("LOG: SD card initialised");
 	}
 }
+*/
 
 // Initialize the MPU
 void initializeMPU() {
@@ -97,19 +103,19 @@ void initializeMPU() {
   Wire.write(0x6B);  // PWR_MGMT_1 register
   Wire.write(0);     // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
-  Serial.begin(9600);
 }
 
 // Setup
 void setup() {                
 	initializePins();
 	initializeSerial();
-	initializeSDcard();
+	//initializeSDcard();
 	initializeMPU();
 }
 
-// Log data record
-void logData (String message) {
+/*
+// Log data record USE_SD = true
+void logData (String message) {	
 	File dataFile = SD.open(LOG_FILE, FILE_WRITE);
 
 	if (dataFile) {
@@ -120,8 +126,17 @@ void logData (String message) {
 	// if the file isn't open, pop up an error:
 	else {
 		Serial.println("<log message='Error opening LOG.TXT...'/>");
+		Serial.println(message);
 	}
 } 
+*/
+
+// Log data record USE_SD = fales
+void logData (String message) {	
+	String line = "<log message='";
+	line += message + "'/>";
+	Serial.println(line);
+}
 
 // Process sync button
 int prevSyncState = LOW;
@@ -148,6 +163,9 @@ void processSyncButton() {
 
 // Process sleep button
 int prevSleepState = LOW;
+int AcStep = 0;
+int AcCount = 0;
+long AcTotal = 0;
 void processSleepButton() {
 	// Only one state at a time!
 	if (mode == MODE_NONE || mode == MODE_SLEEP) {		
@@ -155,12 +173,17 @@ void processSleepButton() {
 		if (prevSleepState == LOW && sleepState == HIGH) {
 			if (mode == MODE_NONE) {
 				mode = MODE_SLEEP;
+				AcStep = 0;
+				AcCount = 0;
+				AcTotal = 0;
 				digitalWrite(PIN_LED_SLEEP, HIGH);
 				logData("LOG: Sleep button was pressed, sleep mode enabled");
 			} else if (mode == MODE_SLEEP) {
 				mode = MODE_NONE;
 				digitalWrite(PIN_LED_SLEEP, LOW);
+				analogWrite(PIN_LED_ACTIVE, 0);
 				logData("LOG: Sleep button was pressed, sleep mode disabled");
+				logData(summariseSleepData());
 			}
 			// Wait for button to come to rest again
 			delay (REST);
@@ -169,12 +192,50 @@ void processSleepButton() {
 
 }
 
-// Process sleep data
-void processSleepData() {
-
+// Summarise sleep data
+String summariseSleepData() {
+	String sleepData = "SLEEP: duration=";
+	sleepData+=AcCount;
+	sleepData+=", activity=";
+	sleepData+=AcTotal;
+	return sleepData;
 }
 
-// Read SD Card data and send line by line
+// Process sleep data
+void processSleepData() {
+	int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+	// Initialise read from MPU
+	Wire.beginTransmission(MPU_ADDRESS);
+	Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+	Wire.endTransmission(false);
+	Wire.requestFrom(MPU_ADDRESS,14,true);  // request a total of 14 registers
+
+	// Perform read from MPU
+	AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+	AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+	AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+	Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+	GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+	GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+	GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+	// Normalise data
+	AcX = AcX / 100;
+	AcY = AcY / 100;
+	if (AcX < 0) AcX = AcX * -1;
+	if (AcY < 0) AcY = AcY * -1;
+	AcCount+=1;
+	AcStep=(AcX+AcY)/2;
+	AcTotal+=AcStep;
+
+	// Set activity LED
+	analogWrite(PIN_LED_ACTIVE, AcStep);
+	delay(REST);
+}
+
+/*
+// Read SD Card data and send line by line: USE_SD=true
 void sendData() {
 	Serial.println("<log message='Sending SD card data'/>");
 	File dataFile = SD.open(LOG_FILE, FILE_READ);
@@ -193,13 +254,28 @@ void sendData() {
 		Serial.println("<log message='Error opening LOG.TXT...'/>");
 	}
 }
+*/
 
+// Read SD Card data and send line by line: USE_SD=true
+void sendData() {
+	Serial.println("<log message='Sending sleep data'/>");
+	Serial.println("<cmd name='data'>");
+	Serial.println(summariseSleepData());
+	Serial.println("</cmd>");
+}
+
+/*
 // Reset SD Card data
 void resetDevice() {
 	Serial.println("<log message='Reset SD card data'/>");
 	if (!SD.remove (LOG_FILE)) {
 		Serial.println("<log message='Error deleting LOG.TXT...'/>");
 	}
+}
+*/
+
+// Reset SD Card data - dummy
+void resetDevice() {
 }
 
 // Perform sleep processing
